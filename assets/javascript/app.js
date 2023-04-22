0c65705bbe1eb8c2cb668822e666f27f0bdd68fd
Matt Lombard <ncmattlombard@gmail.com>
2023-04-22 16:43:29 -0400

    added the event listener for search weather and created the searche weather function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,3 +4,22 @@ const searchInput = document.getElementById('search-input');
 const currentWeather = document.getElementById('current-weather');
 const forecast = document.getElementById('forecast');
 const searchHistory = document.getElementById('search-history');
+
+searchForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const cityName = searchInput.value;
+  searchWeather(cityName);
+  addToSearchHistory(cityName);
+});
+
+function searchWeather(cityName) {
+  const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=imperial`;
+  const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${API_KEY}&units=imperial`;
+  fetch(currentWeatherUrl)
+    .then((response) => response.json())
+    .then((data) => displayCurrentWeather(data));
+
+  fetch(forecastUrl)
+    .then((response) => response.json())
+    .then((data) => displayForecast(data));
+}
