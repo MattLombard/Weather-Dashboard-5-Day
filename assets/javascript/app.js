f45c9fa6b46034442e43c6b2bc76793a7dec8ed0
Matt Lombard <ncmattlombard@gmail.com>
2023-04-22 15:50:30 -0400

    created the layout for my html page and started on the javascript file

---
+++ assets/javascript/app.js
@@ -0,0 +1,6 @@
+const API_KEY = '844b299e4bc040c76b23f3362057e900';
+const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
+const currentWeather = document.getElementById('current-weather');
+const forecast = document.getElementById('forecast');
+const searchHistory = document.getElementById('search-history');
